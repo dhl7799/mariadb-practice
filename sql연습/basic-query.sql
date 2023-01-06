fb784b0da2c77b5773e94a862e2d3a080915c14b
dhl7799 <donghyundhl@naver.com>
2023-01-06 16:32:36 +0900

    first commit

---
+++ sql연습/basic-query.sql
@@ -0,0 +1,36 @@
+select version(), current_date, now() from dual;
+
+-- 수학함수, 사칙 연산도 된다.
+select sin(pi() / 4), 1 + 2 * 3 - 4 / 5 from dual;
+
+-- 대소문자 구분 안한다.
+sElect VERSION(), current_DATE, NOW() froM dual;
+
+-- table 생성: DML
+create table pet (
+	name varchar(100),
+	owner varchar(20),
+	species varchar(20),
+	gender char(1),
+	birth date,
+	death date
+);
+
+-- schema 확인
+describe pet;
+desc pet;
+
+-- table 삭제
+-- drop table pet;
+show tables;
+
+-- insert: DML(C)
+insert
+  into pet
+values ('성탄이', '안대혁', 'dog', 'm', '2007-12-25', null);
+insert
+  into pet
+values ('성탄이2', '김대혁', 'dog', 'm', '2008-12-25', null);
+
+select * from pet;
+    
